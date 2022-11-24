cf637668cb009f8419434b5f6b6b27b3888f4305
karthikeya <karthikeyakotegar@gmail.com>
2022-11-24 11:52:25 +0530

    top arrow

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import { motion } from "framer-motion"
+import Link from 'next/link';
 
 type Props = {}
 
 export default function Header({ }: Props) {
     return (
-        <header className='sticky top-0 flex items-start justify-between p-5 max-w-7xl mx-auto z-20 xl:items-center'>
+        <div className='sticky top-0 flex items-start justify-between p-5 max-w-7xl mx-auto z-20 xl:items-center'>
             <motion.div
                 initial={{
                     x: -500,
@@ -39,30 +40,32 @@ export default function Header({ }: Props) {
                 />
             </motion.div>
 
-            <motion.div
-                initial={{
-                    x: 500,
-                    opacity: 0,
-                    scale: 0.5
-                }}
-                animate={{
-                    x: 0,
-                    opacity: 1,
-                    scale: 1
-                }}
-                transition={{
-                    duration: 1.5 
-                }}
-                className='flex flex-row items-center text-gray-300 cursor-pointer'>
-                <SocialIcon
-                    network='email'
-                    fgColor='gray'
-                    bgColor='transparent'
-                    className='cursor-pointer'
-                />
+            <Link href='#contact'>
+                <motion.div
+                    initial={{
+                        x: 500,
+                        opacity: 0,
+                        scale: 0.5
+                    }}
+                    animate={{
+                        x: 0,
+                        opacity: 1,
+                        scale: 1
+                    }}
+                    transition={{
+                        duration: 1.5
+                    }}
+                    className='flex flex-row items-center text-gray-300 cursor-pointer'>
+                    <SocialIcon
+                        network='email'
+                        fgColor='gray'
+                        bgColor='transparent'
+                        className='cursor-pointer'
+                    />
 
-                <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
-            </motion.div>
-        </header>
+                    <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+                </motion.div>
+            </Link>
+        </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import About from '../components/About'
 import ContactMe from '../components/ContactMe'
 import Header from '../components/Header'
@@ -48,6 +49,17 @@ export default function Home() {
       <section id='contact' className='snap-start'>
         <ContactMe />
       </section>
+
+      <Link href='#hero'>
+        <footer className='sticky bottom-5 w-full cursor-pointer'>
+          <div className='flex items-center justify-center'>
+            {/* <img className='h-10 w-10 rounded-full filter grayscale-0' src="" alt="" /> */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 rounded-full hover:bg-[#f7f019]/80 hover:text-black">
+              <path fillRule="evenodd" d="M11.47 4.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 6.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5zm.53 7.59l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 12.31z" clipRule="evenodd" />
+            </svg>
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
