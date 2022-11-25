ef81f344c30507c28456eb6fb32e3f58f2b8f8ac
karthikeya <karthikeyakotegar@gmail.com>
2022-11-25 11:18:55 +0530

    api to get data from Sanity

---
+++ sanity.ts
@@ -0,0 +1,16 @@
+import {createClient, groq} from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
+
+export const config:any = {
+    dataset : process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion : process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2022-11-16",
+    useCdn: process.env.NODE_ENV === 'production'
+
+}
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) => {
+    createImageUrlBuilder(config).image(source)
+}
---
+++ utils/fetchSkills.ts
@@ -0,0 +1,2 @@
+import { Skill } from "../typings";
+
